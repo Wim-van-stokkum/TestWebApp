adba563eec9b9707243ef07bacfc5743e0029789
wvans <wvans@192.168.178.242>
2020-07-02 11:38:35 +0200

    FirstCommit

---
+++ spring-boot-rest-2/src/main/java/com/haiti/kela/controllers/StudentRetrieveController.java
@@ -0,0 +1,27 @@
+package com.haiti.kela.controllers;
+
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.haiti.kela.beans.Student;
+import com.haiti.kela.beans.StudentRegistration;
+
+@Controller
+
+public class StudentRetrieveController {
+
+  @RequestMapping(method = RequestMethod.GET, value="/student/allstudent")
+
+  @ResponseBody
+
+  public List<Student> getAllStudents() {
+
+  return StudentRegistration.getInstance().getStudentRecords();
+
+  }
+
+}
