fe723c2e75eac97632956752179263e1ebca3ee9
wvans <wvans@192.168.178.242>
2020-07-05 15:01:23 +0200

    for war

--- spring-boot-rest-2/src/main/java/com/haiti/kela/server/main/SpringBootRest2Application.java
+++ spring-boot-rest-2/src/main/java/com/haiti/kela/server/main/SpringBootRest2Application.java
@@ -3,9 +3,17 @@ package com.haiti.kela.server.main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"com.haiti"})
-public class SpringBootRest2Application {
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+@SpringBootApplication(scanBasePackages = {"com.haiti"})
+public class SpringBootRest2Application extends SpringBootServletInitializer  {
+
+	@Override
+	   protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	      return application.sources(SpringBootRest2Application.class);
+	   }
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootRest2Application.class, args);
 	}
